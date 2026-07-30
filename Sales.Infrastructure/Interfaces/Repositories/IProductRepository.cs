f65b5a1c1a64adf31c85ab059ed6da73f241b3f5
Yoel 4Devs <Yoellebron35@gmail.com>
2026-07-30 18:20:42 -0400

    feat: Implement GetProducts in the ProductServices

--- Sales.Infrastructure/Interfaces/Repositories/IProductRepository.cs
+++ Sales.Infrastructure/Interfaces/Repositories/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace Sales.Infrastructure.Interfaces.Repositories
     public interface IProductRepository
     {
         public  void Add(Product product);
-        public IQueryable Query();
+        public IQueryable<Product> Query();
         public void Update(Product product);
         public void Delete(Product product);
     }
