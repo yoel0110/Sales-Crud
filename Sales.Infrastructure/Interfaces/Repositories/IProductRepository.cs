75357d5dc1998b3ded70a565a551113f194fc777
Yoel 4Devs <Yoellebron35@gmail.com>
2026-07-30 17:12:46 -0400

    feat: Add products repository

---
+++ Sales.Infrastructure/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sales.Domain.Entities;
+
+namespace Sales.Infrastructure.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        public  void Add(Product product);
+        public IQueryable Query();
+        public void Update(Product product);
+        public void Delete(Product product);
+    }
+}
