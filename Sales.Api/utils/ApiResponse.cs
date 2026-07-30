c4b7d47a4274c974065e8c458d05288154d9dcb8
Yoel 4Devs <Yoellebron35@gmail.com>
2026-07-30 18:52:06 -0400

    feat: Add api layer and api response standard

---
+++ Sales.Api/utils/ApiResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace Sales.Api.utils
+{
+    public class ApiResponse<T> where T : class
+    {
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public T Data { get; set; }
+
+        private ApiResponse(){}
+
+
+        public static ApiResponse<T> SuccessFul(string message, int statusCode, T data)
+        {
+            return new ApiResponse<T>
+            {
+                Data = data,
+                IsSuccess = true,
+                StatusCode = statusCode,
+                Message = message
+            };
+        }
+
+        public static ApiResponse<T> Failure(string message, int statusCode)
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = false,
+                StatusCode = statusCode,
+                Message = message
+            };
+        }
+    }
+}
