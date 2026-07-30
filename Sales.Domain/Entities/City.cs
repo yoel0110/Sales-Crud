c4744b00fe14c00c5cf8d1b2f7e9ea85d6141c80
Yoel 4Devs <Yoellebron35@gmail.com>
2026-07-30 11:29:49 -0400

    feat: Add Domain entities

---
+++ Sales.Domain/Entities/City.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Sales.Domain.Entities;
+
+public  class City
+{
+    public int CityId { get; set; }
+
+    public string CityName { get; set; }
+
+    public int CountryId { get; set; }
+}
